eb8cf89df83d420a62d12e951e8bbb9ebefa865a
vmolash <molash.v@gmail.com>
2020-03-31 01:45:53 -0700

    Homework 3: eventHandler

--- src/Components/Footer/FooterMenuItem.js
+++ src/Components/Footer/FooterMenuItem.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
+
+const onMouseOverMenuItem = (n) => {
+    console.log(n);
+};
 function FooterMenuItem(props) {
+
+
     return (
+
       <li>
-          <a href={props.item.url} className="menu-link">{props.item.title} </a>
+          <a href={props.item.url} onMouseOver={() => onMouseOverMenuItem(props.item.title) }  className="menu-link">{props.item.title} </a>
       </li>
     );
 
--- src/Components/Header/HeaderMenuItem.js
+++ src/Components/Header/HeaderMenuItem.js
@@ -2,9 +2,13 @@ import React from 'react';
 import Logo from "./Logo";
 
 function HeaderMenuItem(props) {
+    const onPointerOverMenuItem = (n) => {
+        console.log(n);
+    }
+
     return (
       <li className="header">
-          <a href={props.item.url} className='menu-link'>{props.item.title}</a>
+          <a href={props.item.url} onPointerOver={() => onPointerOverMenuItem(props.item.title)} className='menu-link'>{props.item.title}</a>
       </li>
     );
 
